c36662fc1f1ef83de23e7b1df30bb23799bd0d92
Lucas M <lucassanntos01@gmail.com>
2021-11-24 08:04:05 -0200

    Style Color Slider

--- src/Palette.js
+++ src/Palette.js
@@ -2,7 +2,7 @@ import React, { Component } from "react";
 import ColorBox from './ColorBox';
 import "./Palette.css";
 import Slider from "rc-slider";
-import "rc-slider/assets/index.css";
+// import "rc-slider/assets/index.css"; not using anymore bc rc-slider now use props to style the slider. 
 
 
 class Palette extends Component {
@@ -17,18 +17,31 @@ class Palette extends Component {
   render() {
     const { colors } = this.props.palette;
     const { level } = this.state;
-  	const colorBoxes = colors[this.state.level].map(color => ( //n entendi como acessa cores em 100/200 etc
+  	const colorBoxes = colors[level].map(color => ( //n entendi como acessa cores em 100/200 etc
   		<ColorBox background={color.hex} name={color.name} />
   	))
     return (
       <div className="Palette">
-        <Slider 
-          defaultValue={this.state.level} 
-          min={100} 
-          max={900}
-          step={100}
-          onAfterChange={this.changeLevel}
-         />
+        <div className="slider">
+           <Slider
+              defaultValue={level} 
+              min={100} 
+              max={900} 
+              step={100}
+              onAfterChange={this.changeLevel}  
+              trackStyle={{ backgroundColor: 'transparent' }} 
+              handleStyle={{
+                borderColor: 'green',
+                height: 13,
+                width: 13,
+                marginLeft: -7,
+                marginTop: -3,
+                backgroundColor: 'green',
+                boxShadow: 'none',
+              }} 
+              railStyle={{height: 8}}
+            />
+        </div>
         {/*Navbar goes here */}
   	  	<div className="Palette-colors">{colorBoxes}</div>
         {/* footer eventually  */}
