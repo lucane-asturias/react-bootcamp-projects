5002336a47251b31b5b2d7efb9696358b6f9246b
Lucas M <lucassanntos01@gmail.com>
2021-11-26 00:05:53 -0200

    Add React Router

--- src/App.js
+++ src/App.js
@@ -1,15 +1,22 @@
 import React, { Component } from "react";
+import { Route, Switch } from "react-router-dom";
 import Palette from "./Palette";
+import PaletteList from "./PaletteList";
 import seedColors from "./seedColors";
 import { generatePalette } from "./colorHelpers";
 
 class App extends Component {
   render() {
     return (
-      <div className="">
-        {/*<Palette {...seedColors[4]} /> */}
-        <Palette palette={generatePalette(seedColors[4])} />
-      </div>
+      <Switch>
+        <Route exact path="/" render={() => <PaletteList />} />
+        <Route exact path="/palette/:id" render={() => <h1>INDIVIDUAL PALETTE</h1>} />
+      </Switch>
+
+      // <div className="">
+      //   {/*<Palette {...seedColors[4]} /> */}
+      //   <Palette palette={generatePalette(seedColors[4])} />
+      // </div>
     );
   }
 }
---
+++ src/PaletteList.js
@@ -0,0 +1,16 @@
+import React, { Component } from "react";
+
+class PaletteList extends Component {
+  constructor(props) {
+    super(props);
+  }
+  render() {
+    return (
+      <div className="PaletteList">
+        <h1>React Colors</h1>
+      </div>
+    );
+  }
+}
+
+export default PaletteList;
