79243e6c56ccb550524b7564716e96378132a330
Lucas M <lucassanntos01@gmail.com>
2021-12-04 14:21:51 -0200

    Make form and navbar responsive

--- src/styles/DraggableColorBoxStyles.js
+++ src/styles/DraggableColorBoxStyles.js
@@ -1,3 +1,5 @@
+import sizes from "./sizes";
+
 const styles = {
   root: {
     width: "20%",
@@ -10,20 +12,33 @@ const styles = {
     "&:hover $deleteIcon": { //easiest way to style while hovering is selecting the class with $ sign
       color: "white",
       transform: "scale(1.5)"
+    },
+    [sizes.down("lg")]: {
+      width: "25%",
+      height: "20%"
+    },
+    [sizes.down("md")]: {
+      width: "50%",
+      height: "10%"
+    },
+    [sizes.down("sm")]: {
+      width: "100%",
+      height: "8"
     }
   },
   boxContent: {
-    "position": "absolute",
-    "padding": "10px",
-    "width":"100%",
-    "left": "0px",
-    "bottom": "0px",
-    "color": "rgba(0, 0, 0, 0.5)",
-    "letterSpacing": "1px",
-    "textTransform": "uppercase",
-    "fontSize": "12px",
-    "display": "flex",
-    "justifyContent": "space-between"
+    position: "absolute",
+    padding: "10px",
+    width:"100%",
+    left: "0px",
+    bottom: "0px",
+    padding: '0 0.7rem 0.5rem 1rem',
+    color: "rgba(0, 0, 0, 0.5)",
+    letterSpacing: "1px",
+    textTransform: "uppercase",
+    fontSize: "12px",
+    display: "flex",
+    justifyContent: "space-between"
   },
   deleteIcon: {
     transition: "all 0.3s ease-in-out"
