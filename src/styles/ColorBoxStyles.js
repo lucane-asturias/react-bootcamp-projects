75cea0f3eb0512822f84fd97ed3d268d4c910c50
Lucas M <lucassanntos01@gmail.com>
2021-12-04 12:37:36 -0200

    Make ColorBox responsive

--- src/styles/ColorBoxStyles.js
+++ src/styles/ColorBoxStyles.js
@@ -12,7 +12,7 @@ export default {
     display: "inline-block",
     position: "relative",
     cursor: "pointer",
-    
+
     /*margin-top: -3.8px;  this was for wh'ite space. */
     // "&:hover button": { em vez de estilizar todos os botões usando classe dinâmica
     "&:hover $copyButton": {
@@ -21,11 +21,11 @@ export default {
     },
     [sizes.down("lg")]: {
       width: "25%",
-      height: props => (props.showingFullPalette ? "20%" : "50%")
+      height: props => (props.showingFullPalette ? "20%" : "33.3333%")
     },
     [sizes.down("md")]: {
       width: "50%",
-      height: props => (props.showingFullPalette ? "10%" : "50%")
+      height: props => (props.showingFullPalette ? "10%" : "20%")
     },
     [sizes.down("xs")]: {
       width: "100%",
@@ -122,7 +122,10 @@ export default {
         textAlign: "center",
         marginBottom: "0",
         padding: "1rem",
-        textTransform: "uppercase"
+        textTransform: "uppercase",
+        [sizes.down("xs")]: {
+          fontSize: "5rem"
+        },
     },
     "& p": {
         fontSize: "2rem",
