c2724f2c4fbbc02fa57580e3f65b5eb0c8ace0b5
Lucas M <lucassanntos01@gmail.com>
2021-12-04 08:12:46 -0200

    Add DeleteIcon to MiniPalette

--- src/MiniPalette.js
+++ src/MiniPalette.js
@@ -2,6 +2,7 @@ import React from "react";
 import { withStyles } from "@material-ui/styles";
 import { Emoji } from 'emoji-mart';
 import styles from "./styles/MiniPaletteStyles";
+import DeleteIcon from "@material-ui/icons/Delete";
 
 
 function MiniPalette(props) { //functional component that accepts a prop (in this case from PalleteList)
@@ -16,10 +17,16 @@ function MiniPalette(props) { //functional component that accepts a prop (in thi
     ));
     return (
         <div className={classes.root} onClick={props.handleClick}>
-            <div className={classes.colors}>{miniColorBoxes}</div>
-                <h5 className={classes.title}>
-                    {paletteName} <Emoji emoji={emoji} set="google" size={18} />
-                </h5>
+        <div className={classes.delete}>
+            <DeleteIcon 
+                className={classes.deleteIcon} 
+                style={{ transition: "all 0.3s ease-in-out"}} //override built-in styles from mu
+            />
+        </div>
+        <div className={classes.colors}>{miniColorBoxes}</div>
+            <h5 className={classes.title}>
+                {paletteName} <Emoji emoji={emoji} set="google" size={18} />
+            </h5>
         </div>
     );
 }
--- src/styles/MiniPaletteStyles.js
+++ src/styles/MiniPaletteStyles.js
@@ -6,8 +6,9 @@ export default {
         padding: "0.5rem",
         position: "relative",
         overflow: "hidden",
-        "&:hover": {
-            cursor: "pointer"
+        cursor: "pointer",
+        "&:hover $deleteIcon": {
+            opacity: 1
         }
     },
     colors: {
@@ -38,5 +39,18 @@ export default {
         margin: "0 auto",
         position: "relative",
         marginBottom: "-4px"
+    },
+    delete: {},
+    deleteIcon: {
+        color: "#eee",
+        backgroundColor: "#eb3d36",
+        width: "20px",
+        height: "20px",
+        position: "absolute",
+        right: "0px",
+        top: "0px",
+        padding: "10px",
+        zIndex: "10",
+        opacity: "0"
     }
 };
