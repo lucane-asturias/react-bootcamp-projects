83f148446d1e970530eff5a26e6065864e7ef22a
Lucas M <lucassanntos01@gmail.com>
2022-02-25 22:07:45 -0300

    Updates

---
+++ precache-manifest.ab9148657903f2016d3e8caa3965b72a.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "259374b825d95833dde1",
+    "url": "/react-color-palette/static/css/main.c3a712ed.chunk.css"
+  },
+  {
+    "revision": "259374b825d95833dde1",
+    "url": "/react-color-palette/static/js/main.d8968d76.chunk.js"
+  },
+  {
+    "revision": "16aa53162b71723401c3",
+    "url": "/react-color-palette/static/js/runtime~main.4d956a40.js"
+  },
+  {
+    "revision": "3c361d0c5e6e78be3c09",
+    "url": "/react-color-palette/static/css/2.8e0c0844.chunk.css"
+  },
+  {
+    "revision": "3c361d0c5e6e78be3c09",
+    "url": "/react-color-palette/static/js/2.728347f1.chunk.js"
+  },
+  {
+    "revision": "0102362494c104edb64d",
+    "url": "/react-color-palette/static/js/3.3432eb6f.chunk.js"
+  },
+  {
+    "revision": "0d8a90b870a3b2e3faf8f734469f902c",
+    "url": "/react-color-palette/static/media/bg.0d8a90b8.svg"
+  },
+  {
+    "revision": "9ebef64e4011771d73ab0d075eb90fa4",
+    "url": "/react-color-palette/index.html"
+  }
+];
